d9cb124a3c4d09f1803dbafabc50e0ec8c94377c
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-18 22:38:18 +0200

    Update 01_temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,7 +1,14 @@
-def ftoc fahrenheitValue
-    return (fahrenheitValue -32)*5/9
+def ftoc(fahrenheit)
+  temperature = fahrenheit - 32
+  celsius = (temperature) *5 / 9
+  return celsius
+  (fahrenheit - 32) * 5 / 9
 end
 
-def ctof celsiusValue
-    return (celsiusValue.to_f * 9/5)+32
+def ctof(celsius)
+  temperature = celsius.to_f
+  temperature = (temperature) *9 / 5
+  fahrenheit = (temperature) + 32
+  return fahrenheit
+  (celsius.to_f) * 9 / 5 + 32
 end
